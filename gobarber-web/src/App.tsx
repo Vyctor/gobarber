732e4815e7d0afdad823944a53d5b46844558c72
Vyctor <dev.vyctor@gmail.com>
2020-05-18 17:51:00 -0300

    Rotas, toast e autenticação

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
+import Routes from './routes';
+
+import { AppProvider } from './hooks';
 
 const App: React.FC = () => (
-  <>
-    <SignIn />
+  <BrowserRouter>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
     <GlobalStyle />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
--- gobarber-web/src/pages/SignIn/styles.ts
+++ gobarber-web/src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signInBackgroundImg from '../../assets/sign-in-background.png';
@@ -14,11 +14,35 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  place-content: center;
-
+  justify-content: center;
   width: 100%;
   max-width: 70rem;
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromLeft} 2s;
 
   form {
     margin: 8rem 0;
@@ -58,10 +82,4 @@ export const Content = styled.div`
       color: ${shade(0.2, '#ff9000')};
     }
   }
-`;
-
-export const Background = styled.div`
-  flex: 1;
-  background: url(${signInBackgroundImg}) no-repeat center;
-  background-size: cover;
 `;
