5d36af7a6dda9769d5ab866c48f95495f4cd133f
Vyctor <dev.vyctor@gmail.com>
2020-08-07 00:36:56 -0300

    listando horarios disponiveis

--- back-end/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ back-end/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -2,6 +2,7 @@ import { getRepository, Repository, Raw } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../../../dtos/ICreateAppointmentDTO';
 
@@ -43,6 +44,28 @@ class AppointmentsRepository implements IAppointmentsRepository {
         date: Raw(
           dateFieldName => `
         to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+        ),
+      },
+    });
+
+    return appointments;
+  }
+
+  public async findAllInDayFromProvider({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const parsedMonth = String(month).padStart(2, '0');
+    const parsedDay = String(day).padStart(2, '0');
+
+    const appointments = this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateFieldName => `
+        to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
     });
--- back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,11 +1,18 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProvider from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
+
   findByDate(date: Date): Promise<Appointment | undefined>;
+
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProvider,
+  ): Promise<Appointment[]>;
+
+  findAllInDayFromProvider(
+    data: IFindAllInDayFromProviderDTO,
   ): Promise<Appointment[]>;
 }
