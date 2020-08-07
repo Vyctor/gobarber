45ef37e795bea15135a0bfd8b0529751dbe66b61
Vyctor <dev.vyctor@gmail.com>
2020-08-06 23:47:28 -0300

    filtrando agendamentos por mês

--- back-end/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ back-end/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,7 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
-import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import { getRepository, Repository, Raw } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../../../dtos/ICreateAppointmentDTO';
 
 class AppointmentsRepository implements IAppointmentsRepository {
@@ -27,6 +28,26 @@ class AppointmentsRepository implements IAppointmentsRepository {
     await this.ormRepository.save(appointent);
 
     return appointent;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const parsedMonth = String(month).padStart(2, '0');
+
+    const appointments = this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateFieldName => `
+        to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+        ),
+      },
+    });
+
+    return appointments;
   }
 }
 
