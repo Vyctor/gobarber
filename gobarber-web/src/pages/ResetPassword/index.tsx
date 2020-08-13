21bf247dbd5affe76a4fcf2975cb5e50cf173f9d
Vyctor <dev.vyctor@gmail.com>
2020-08-12 22:11:42 -0300

    implementado redefinicao

--- gobarber-web/src/pages/ResetPassword/index.tsx
+++ gobarber-web/src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import { FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErros';
@@ -14,6 +14,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, Background, AnimationContainer } from './styles';
+import api from '../../services/apiClient';
 
 interface ResetPasswordFormData {
   password: string;
@@ -25,6 +26,9 @@ const ResetPassword: React.FC = () => {
 
   const { addToast } = useToast();
   const history = useHistory();
+  const location = useLocation();
+
+  console.log(location);
 
   const handleSubmit = useCallback(
     async (data: ResetPasswordFormData) => {
@@ -44,6 +48,19 @@ const ResetPassword: React.FC = () => {
 
         await schema.validate(data, { abortEarly: false });
 
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
+        });
+
         history.push('/signin');
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
@@ -58,7 +75,7 @@ const ResetPassword: React.FC = () => {
         });
       }
     },
-    [addToast, history],
+    [addToast, history, location.search],
   );
 
   return (
